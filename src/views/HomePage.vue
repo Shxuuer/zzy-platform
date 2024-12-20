b9b385978603bbaacec253070c8824c5928a21a6
Shxuuer <shenxu@bupt.edu.cn>
2024-12-20 10:57:02 +0800

    123

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -10,12 +10,32 @@ export default {
   data() {
     return {
       msg: [],
+      filter: {},
+      filter_list: {},
+      r_msg: [],
     };
   },
   methods: {
     updateInfo() {
       axios.get("/get_info").then((res) => {
         this.msg = res.data;
+        // 获取所有可能的location筛选条件
+        this.filter_list = { location: [] };
+        for (let i of this.msg) {
+          if (!this.filter_list.location.includes(i.location)) {
+            this.filter_list.location.push(i.location);
+          }
+        }
+      });
+    },
+    handleClick(type, key) {
+      this.filter[type] = key;
+      this.r_msg = this.msg.filter((i) => {
+        return (
+          (this.filter.location === undefined ||
+            i.location === this.filter.location) &&
+          (this.filter.type === undefined || i.type === this.filter.type)
+        );
       });
     },
   },
@@ -28,19 +48,58 @@ export default {
 
 <template>
   <div class="main">
-    <template v-for="i in msg" :key="i.id">
-      <InfoTable :msg="i" class="item" />
-    </template>
+    <div class="title">
+      <h1>预警信息</h1>
+    </div>
+    <div class="info">
+      <!-- 筛选 -->
+      <div>
+        <a-dropdown>
+          <a-button> {{ filter.location || "地点" }} </a-button>
+          <template #overlay>
+            <a-menu @click="handleClick('location', $event.key)">
+              <a-menu-item
+                v-for="i in filter_list.location"
+                :key="i"
+                :value="i"
+                >{{ i }}</a-menu-item
+              >
+            </a-menu>
+          </template>
+        </a-dropdown>
+      </div>
+      <template v-for="i in msg" :key="i.id">
+        <InfoTable :msg="i" class="item" />
+      </template>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .main {
+  position: relative;
+  width: 100%;
+}
+
+.info {
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100%;
   padding-bottom: 20px;
+  position: relative;
+  top: 60px;
+}
+
+.title {
+  line-height: 60px;
+  height: 60px;
+  z-index: 2;
+  font-size: 1.3em;
+  font-weight: bold;
+  position: fixed;
+  background-color: #e3e3e3;
+  width: 100%;
+  padding-left: 30px;
 }
 
 .item {
