aa3438bd328460a591e7d1e09a35bd1529ff2efe
Shxuuer <shenxu@bupt.edu.cn>
2024-12-21 10:41:48 +0800

    修改ui

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -47,13 +47,21 @@ export default {
 </script>
 
 <template>
+  <div class="tmp">
+    <div>警告</div>
+    <div>有2条未处理预警信息！</div>
+    <div>查看</div>
+  </div>
   <div class="main">
     <div class="title">
-      <h1>预警信息</h1>
+      <img src="/logo.png" />
+      <h1 style="line-height: 60px; height: 40px">预警信息查询</h1>
     </div>
     <div class="info">
       <!-- 筛选 -->
-      <div>
+      <div style="margin: 20px">筛选器</div>
+
+      <div style="display: flex; flex-direction: row">
         <a-dropdown>
           <a-button> {{ filter.location || "地点" }} </a-button>
           <template #overlay>
@@ -67,6 +75,23 @@ export default {
             </a-menu>
           </template>
         </a-dropdown>
+        <a-dropdown>
+          <a-button style="margin-left: 10px">
+            {{ filter.type || "类型" }}
+          </a-button>
+          <template #overlay>
+            <a-menu @click="handleClick('type', $event.key)">
+              <a-menu-item key="1" :value="1">类型1</a-menu-item>
+              <a-menu-item key="2" :value="2">类型2</a-menu-item>
+              <a-menu-item key="3" :value="3">类型3</a-menu-item>
+            </a-menu>
+          </template>
+        </a-dropdown>
+        <a-time-picker
+          style="margin-left: 10px"
+          placeholder="选择时间范围"
+          @change="handleClick('time', $event)"
+        ></a-time-picker>
       </div>
       <template v-for="i in msg" :key="i.id">
         <InfoTable :msg="i" class="item" />
@@ -76,6 +101,50 @@ export default {
 </template>
 
 <style scoped>
+.tmp {
+  position: fixed;
+  top: 200px;
+  right: 10px;
+  width: 250px;
+  height: 130px;
+  background-color: red;
+  border-radius: 10px;
+  color: #e3e3e3;
+  font-weight: 500;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 2;
+  padding: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.tmp div:first-child {
+  font-size: 1.5em;
+  width: 90%;
+  text-align: center;
+  margin-top: 10px;
+  border-bottom: 1px solid #e3e3e3;
+}
+
+.tmp div:nth-child(2) {
+  font-size: 1.1em;
+  text-align: center;
+  margin-top: 10px;
+}
+
+.tmp div:last-child {
+  font-size: 1.1em;
+  text-align: center;
+  width: 25%;
+  margin-top: 10px;
+  color: #3e3e3e;
+  background-color: #e3e3e3;
+  border-radius: 5px;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
 .main {
   position: relative;
   width: 100%;
@@ -91,15 +160,27 @@ export default {
 }
 
 .title {
+  display: flex;
+  flex-direction: row;
   line-height: 60px;
   height: 60px;
   z-index: 2;
-  font-size: 1.3em;
+  font-size: 1em;
   font-weight: bold;
   position: fixed;
   background-color: #e3e3e3;
   width: 100%;
   padding-left: 30px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+  top: 0;
+  align-items: center;
+}
+
+.title img {
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
 }
 
 .item {
